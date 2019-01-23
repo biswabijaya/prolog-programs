3c44ed4eaab53aca0cd831579981ce4124bfd2e1
Biswabijaya Samal <me@biswabijaya.com>
2019-01-22 22:45:13 -0800

    Add files via upload

---
+++ family-tree.pl
@@ -0,0 +1,45 @@
+male(john).
+male(bob).
+male(bill).
+male(ron).
+male(jeff).
+
+female(mary).
+female(sue).
+female(nancy).
+
+mother(mary, sue).
+mother(mary, bill).
+mother(sue, nancy).
+mother(sue, jeff).
+mother(jane, ron).
+
+father(john, sue).
+father(john, bill).
+father(bob, nancy).
+father(bob, jeff).
+father(bill, ron).
+
+sibling(bob,bill).
+sibling(sue,bill).
+sibling(nancy,jeff).
+sibling(nancy,ron).
+sibling(jeff,ron).
+
+
+
+sibling(X, Y) :-
+      parent(Z, X),
+      parent(Z, Y),
+      X \= Y.
+
+sister(X, Y) :-
+      sibling(X, Y),
+      female(X).
+
+brother(X, Y) :-
+      sibling(X, Y),
+      male(X).
+
+parent(Z,Y) :- father(Z,Y).
+parent(Z,Y) :- mother(Z,Y).
